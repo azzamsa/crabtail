53e9ad869d65fc2e9cd79c61b2a4904b83537e71
azzamsa <me@azzamsa.com>
2021-03-23 02:57:18 +0700

    feat: integrate seed

---
+++ configs/postcss.config.js
@@ -0,0 +1,31 @@
+const postcssRustHelpers = require("../scripts/postcss_rust_helpers");
+
+module.exports = ( ctx ) => {
+  // we want to filter out unused css classes in production mode
+  // NOTE: ctx.env is set in postcss-cli `--env` configs
+  const usedCssClasses =
+        ctx.env === "production"
+        ? postcssRustHelpers.getUsedCssClasses()
+        : null;
+
+  console.log("postcss ctx.env: " + ctx.env);
+  return {
+    plugins: [
+      require("tailwindcss")('configs/tailwind.config.js'),
+      require("autoprefixer"),
+      require("postcss-typed-css-classes")({
+        output_filepath: "src/generated/css_classes.rs",
+        generator: "rust",
+        filter: class_ => {
+          if (ctx.env === "production") {
+            return usedCssClasses.has(
+              postcssRustHelpers.escapeClassName(class_)
+            );
+          } else {
+            return true;
+          }
+        }
+      }),
+    ]
+  };
+};
