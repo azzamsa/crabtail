1cc3fd35fb32ffff8e9f41211685a1b3d4c37836
azzamsa <me@azzamsa.com>
2021-03-23 16:30:06 +0700

    refactor: format with prettier

--- configs/postcss.config.js
+++ configs/postcss.config.js
@@ -1,34 +1,30 @@
-const postcssRustHelpers = require("../scripts/postcss_rust_helpers");
+const postcssRustHelpers = require("../scripts/postcss_rust_helpers")
 
-module.exports = ( ctx ) => {
+module.exports = (ctx) => {
   // we want to filter out unused css classes in production mode
   // NOTE: ctx.env is set in postcss-cli `--env` configs
   const usedCssClasses =
-        ctx.env === "production"
-        ? postcssRustHelpers.getUsedCssClasses()
-        : null;
+    ctx.env === "production" ? postcssRustHelpers.getUsedCssClasses() : null
 
-  console.log("postcss ctx.env: " + ctx.env);
+  console.log("postcss ctx.env: " + ctx.env)
   return {
     // allow inline comment in SASS
-    syntax: 'postcss-scss',
+    syntax: "postcss-scss",
     plugins: [
       require("postcss-import"),
-      require("tailwindcss")('configs/tailwind.config.js'),
+      require("tailwindcss")("configs/tailwind.config.js"),
       require("autoprefixer"),
       require("postcss-typed-css-classes")({
         output_filepath: "src/generated/css_classes.rs",
         generator: "rust",
-        filter: class_ => {
+        filter: (class_) => {
           if (ctx.env === "production") {
-            return usedCssClasses.has(
-              postcssRustHelpers.escapeClassName(class_)
-            );
+            return usedCssClasses.has(postcssRustHelpers.escapeClassName(class_))
           } else {
-            return true;
+            return true
           }
-        }
+        },
       }),
-    ]
-  };
-};
+    ],
+  }
+}
--- scripts/postcss_rust_helpers.js
+++ scripts/postcss_rust_helpers.js
@@ -1,79 +1,77 @@
-const path = require('path')
-const findFiles = require('find');
-const fs = require('fs');
+const path = require("path")
+const findFiles = require("find")
+const fs = require("fs")
 
 // Helpers are used by `configs/postcss.config.js`.
 
 module.exports = {
-    escapeClassName: escapeClassName,
+  escapeClassName: escapeClassName,
 
-    /**
-     * Search in Rust and Handlebars files for CSS classes.
-     *
-     * @returns Set of class names
-     */
-    getUsedCssClasses: function() {
-        return new Set([
-            ...getUsedCssClassesInRust(),
-        ])
-    }
+  /**
+   * Search in Rust and Handlebars files for CSS classes.
+   *
+   * @returns Set of class names
+   */
+  getUsedCssClasses: function () {
+    return new Set([...getUsedCssClassesInRust()])
+  },
 }
 
 // https://doc.rust-lang.org/book/appendix-01-keywords.html
-function getKeywords () {
-    return [
-        'as',
-        'break',
-        'const',
-        'continue',
-        'crate',
-        'dyn',
-        'else',
-        'enum',
-        'extern',
-        'false',
-        'fn',
-        'for',
-        'if',
-        'impl',
-        'in',
-        'let',
-        'loop',
-        'match',
-        'mod',
-        'move',
-        'mut',
-        'pub',
-        'ref',
-        'return',
-        'Self',
-        'self',
-        'static',
-        'struct',
-        'super',
-        'trait',
-        'true',
-        'type',
-        'unsafe',
-        'use',
-        'where',
-        'while',
+function getKeywords() {
+  return [
+    "as",
+    "break",
+    "const",
+    "continue",
+    "crate",
+    "dyn",
+    "else",
+    "enum",
+    "extern",
+    "false",
+    "fn",
+    "for",
+    "if",
+    "impl",
+    "in",
+    "let",
+    "loop",
+    "match",
+    "mod",
+    "move",
+    "mut",
+    "pub",
+    "ref",
+    "return",
+    "Self",
+    "self",
+    "static",
+    "struct",
+    "super",
+    "trait",
+    "true",
+    "type",
+    "unsafe",
+    "use",
+    "where",
+    "while",
 
-        'abstract',
-        'async',
-        'become',
-        'box',
-        'do',
-        'final',
-        'macro',
-        'override',
-        'priv',
-        'try',
-        'typeof',
-        'unsized',
-        'virtual',
-        'yield'
-    ]
+    "abstract",
+    "async",
+    "become",
+    "box",
+    "do",
+    "final",
+    "macro",
+    "override",
+    "priv",
+    "try",
+    "typeof",
+    "unsized",
+    "virtual",
+    "yield",
+  ]
 }
 
 /**
@@ -82,20 +80,20 @@ function getKeywords () {
  * @returns usedCssClasses Set of class names
  */
 function getUsedCssClassesInRust() {
-    const usedCssClasses = new Set;
-    // search in Rust files
-    const files = findFiles.fileSync(/\.rs$/, './src');
-    files.forEach(filePath => {
-        const fileContent = fs.readFileSync(filePath, 'utf8')
-        // example of a used class in Rust code is `C.mb_16`
-        const usedCssClassesInFile = fileContent.match(/C\.[a-z0-9_]+/g) || []
-        usedCssClassesInFile
-            // remove prefix `C.`
-            .map(class_ => class_.substring(2))
-            // add class to set
-            .forEach(class_ => usedCssClasses.add(class_))
-    })
-    return usedCssClasses
+  const usedCssClasses = new Set()
+  // search in Rust files
+  const files = findFiles.fileSync(/\.rs$/, "./src")
+  files.forEach((filePath) => {
+    const fileContent = fs.readFileSync(filePath, "utf8")
+    // example of a used class in Rust code is `C.mb_16`
+    const usedCssClassesInFile = fileContent.match(/C\.[a-z0-9_]+/g) || []
+    usedCssClassesInFile
+      // remove prefix `C.`
+      .map((class_) => class_.substring(2))
+      // add class to set
+      .forEach((class_) => usedCssClasses.add(class_))
+  })
+  return usedCssClasses
 }
 
 /**
@@ -106,11 +104,11 @@ function getUsedCssClassesInRust() {
  * @returns string Escaped class name
  */
 function escapeClassName(name) {
-    name = name.replace(/-/g, '_')
-    name = name.replace(/:/g, '__')
-    name = name.replace(/\//g, 'of')
-    if (getKeywords().indexOf(name) > -1) {
-        name += '_'
-    }
-    return name
+  name = name.replace(/-/g, "_")
+  name = name.replace(/:/g, "__")
+  name = name.replace(/\//g, "of")
+  if (getKeywords().indexOf(name) > -1) {
+    name += "_"
+  }
+  return name
 }
