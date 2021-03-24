3feb487caf39efcab35bdcf7dc0c1cb2be74679d
azzamsa <me@azzamsa.com>
2021-03-24 08:45:05 +0700

    fix: can't convert double underline in separate place

--- src/transform.rs
+++ src/transform.rs
@@ -15,15 +15,14 @@ pub fn to_typed(input: &str) -> String {
 }
 
 pub fn to_css(input: &str) -> String {
-    let classes = split::split(input, " ");
+    let classes = split::split(input, ",");
     let classes_underscored = classes
         .iter()
-        .map(|&x| manipulate::replace_all(x, "C.", ""))
-        .map(|x| manipulate::replace_all(&x, "_", "-"))
         .map(|x| manipulate::replace_all(&x, "__", ":"))
-        .map(|x| manipulate::replace_all(&x, ",", ""))
+        .map(|x| manipulate::replace_all(&x, "_", "-"))
+        .map(|x| manipulate::replace_all(&x, "C.", ""))
         .collect::<Vec<_>>();
-    classes_underscored.join(" ")
+    classes_underscored.join("")
 }
 
 #[cfg(test)]
@@ -76,13 +75,6 @@ mod tests {
     }
 
     #[test]
-    fn test_transform2_double_undescore() {
-        let input_string = "C.bg_gray_100, C.text_white, C.font_bold, C.py_2";
-        let expected_result = "bg-gray-100 text-white font-bold py-2".to_string();
-        assert_eq!(to_css(input_string), expected_result);
-    }
-
-    #[test]
     fn test_transform2_one_word() {
         let input_string = "C.flex";
         let expected_result = "flex".to_string();
@@ -97,9 +89,25 @@ mod tests {
     }
 
     #[test]
+    fn test_transform2_double_undescore_separate_place() {
+        let input_string = "C.bg_gray_100, C.text_white, C.font_bold, C.py_2";
+        let expected_result = "bg-gray-100 text-white font-bold py-2".to_string();
+        assert_eq!(to_css(input_string), expected_result);
+    }
+
+    #[test]
     fn test_transform2_with_double_underscore_one_place() {
         let input_string = "C.text_white, C.font_bold, C.py_2, C.hover__bg_blue_light, C.rounded";
         let expected_result = "text-white font-bold py-2 hover:bg-blue-light rounded".to_string();
+        assert_eq!(to_css(input_string), expected_result);
+    }
+
+    #[test]
+    fn test_transform2_with_double_underscore_one_place1() {
+        let input_string =
+            "C.focus__text-white, C.font_bold, C.py_2, C.hover__bg_blue_light C.rounded";
+        let expected_result =
+            "focus:text-white font-bold py-2 hover:bg-blue-light rounded".to_string();
         assert_eq!(to_css(input_string), expected_result);
     }
 }
