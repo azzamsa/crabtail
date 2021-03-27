4c8a045e8018365eb342d7725938b7ba13513a6d
Martin Kavík <martin@kavik.cz>
2021-03-28 00:57:20 +0100

    refactor: main.rs, text_area.rs

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@
 
 mod generated;
 mod icon;
+mod text_area;
 mod transform;
 
 use seed::{prelude::*, *};
+use std::mem;
+use text_area::{TextArea, CssTextArea, TypedTextArea};
 
 use crate::generated::css_classes::C;
 
@@ -16,9 +19,8 @@ use crate::generated::css_classes::C;
 
 fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
     Model {
-        textarea_input: TextArea::generate(&TextAreaType::Css),
-        textarea_output: TextArea::generate(&TextAreaType::Typed),
-        is_swapped: false,
+        text_area_input: Box::new(CssTextArea::new()),
+        text_area_output: Box::new(TypedTextArea::new()),
     }
 }
 
@@ -27,131 +29,35 @@ fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
 // ------ ------
 
 struct Model {
-    textarea_input: TextArea,
-    textarea_output: TextArea,
-    is_swapped: bool,
+    text_area_input: Box<dyn TextArea>,
+    text_area_output: Box<dyn TextArea>,
 }
 
-struct TextArea {
-    label: Option<String>,
-    placeholder: Option<String>,
-    value: Option<String>,
-}
-
-#[derive(PartialEq)]
-enum TextAreaType {
-    Css,
-    Typed,
-}
-
-impl TextArea {
-    fn generate(textarea_type: &TextAreaType) -> Self {
-        if textarea_type == &TextAreaType::Css {
-            Self {
-                label: Some("CSS".to_string()),
-                placeholder: Some("py-2 text-white hover:bg-yellow-500".to_string()),
-                value: Some("".to_string()),
-            }
-        } else {
-            Self {
-                label: Some("Typed".to_string()),
-                placeholder: Some(
-                    "C.py_2, C.text_white, C.hover__bg_yellow_500".to_string(),
-                ),
-                value: Some("".to_string()),
-            }
-        }
-    }
-}
 // ------ ------
 //    Update
 // ------ ------
 
 enum Msg {
     Transform,
-    FirstTextAreaChanged(String),
-    SecondTextAreaChanged(String),
+    TextAreaInputChanged(String),
+    TextAreaOutputChanged(String),
     Swap,
 }
 
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
-    let is_swapped = &mut model.is_swapped;
-    let textarea_input = &mut model.textarea_input;
-    let textarea_output = &mut model.textarea_output;
-
     match msg {
-        Msg::FirstTextAreaChanged(class_input) => {
-            let default = if *is_swapped {
-                TextArea::generate(&TextAreaType::Typed)
-            } else {
-                TextArea::generate(&TextAreaType::Css)
-            };
-            *textarea_input = TextArea {
-                value: Some(class_input),
-                ..default
-            };
+        Msg::TextAreaInputChanged(value) => {
+            model.text_area_input.set_value(value)
         },
-        Msg::SecondTextAreaChanged(class_input) => {
-            let default = if *is_swapped {
-                TextArea::generate(&TextAreaType::Css)
-            } else {
-                TextArea::generate(&TextAreaType::Typed)
-            };
-            *textarea_output = TextArea {
-                value: Some(class_input),
-                ..default
-            }
+        Msg::TextAreaOutputChanged(value) => {
+            model.text_area_output.set_value(value)
         },
         Msg::Swap => {
-            let textarea_input_value_tmp = textarea_input.value.clone();
-            let textarea_output_value_tmp = textarea_output.value.clone();
-
-            let (default_input, default_output) = if *is_swapped {
-                // keep the default
-                *is_swapped = false;
-                (
-                    TextArea::generate(&TextAreaType::Css),
-                    TextArea::generate(&TextAreaType::Typed),
-                )
-            } else {
-                // if not swapped yet
-                // default_input (CSS) -> Typed
-                // default_output (Typed) -> CSS
-                *is_swapped = true;
-                (
-                    TextArea::generate(&TextAreaType::Typed),
-                    TextArea::generate(&TextAreaType::Css),
-                )
-            };
-            *textarea_input = TextArea {
-                value: Some(textarea_output_value_tmp.unwrap_or_else(|| "".to_string())),
-                ..default_input
-            };
-            *textarea_output = TextArea {
-                value: Some(textarea_input_value_tmp.unwrap_or_else(|| "".to_string())),
-                ..default_output
-            };
+            mem::swap(&mut model.text_area_input, &mut model.text_area_output);
         },
         Msg::Transform => {
-            let value = if *is_swapped {
-                transform::to_css(
-                    &textarea_input.value.clone().unwrap_or_else(|| "".to_string()),
-                )
-            } else {
-                // if not swapped yet
-                transform::to_typed(
-                    &textarea_input.value.clone().unwrap_or_else(|| "".to_string()),
-                )
-            };
-            *textarea_output = TextArea {
-                value: Some(value),
-                label: Some(
-                    textarea_output.label.clone().unwrap_or_else(|| "".to_string()),
-                ),
-                placeholder: Some(
-                    textarea_output.placeholder.clone().unwrap_or_else(|| "".to_string()),
-                ),
-            };
+            let input = model.text_area_input.value();
+            model.text_area_output.set_transformed_value(input);
         },
     }
 }
@@ -160,9 +66,8 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 //     View
 // ------ ------
 
-#[allow(clippy::too_many_lines)]
-fn view(model: &Model) -> impl IntoNodes<Msg> {
-    vec![section![
+fn view(model: &Model) -> Node<Msg> {
+    section![
         C![C.bg_main, C.pt_12, C.md__pt_20, C.pb_6, C.px_2, C.md__px_5, C.min_h_screen],
         header![
             C![C.max_w_lg, C.mx_auto],
@@ -179,98 +84,105 @@ fn view(model: &Model) -> impl IntoNodes<Msg> {
                 C.rounded_lg,
                 C.shadow_2xl
             ],
-            section![p![
-                C![C.text_lg, C.text_center, C.text_gray_600, C.pt_0],
-                "Convert your TailwindCSS",
-                i![
-                    C![C.inline_block, C.mx_1, C.align_middle C.w_4, C.h_4],
-                    raw_svg!(icon::get(&icon::Name::Wind))
-                ],
-                "to typed Rust",
-                i![
-                    C![C.inline_block, C.mx_1, C.align_middle C.w_4, C.h_4],
-                    raw_svg!(icon::get(&icon::Name::Crab))
-                ],
-            ]],
-            section![
-                C![C.flex, C.flex_col, C.mt_10],
-                div![
-                    id!["input"],
-                    C![C.mb_6, C.pt_3, C.rounded, C.bg_gray_200],
-                    label![
-                        C![C.input_label],
-                        &model.textarea_input.label.as_deref().unwrap_or("")
-                    ],
-                    textarea![
-                        attrs! {
-                            At::Type => "text",
-                            At::Placeholder => &model.textarea_input.placeholder.as_deref().unwrap_or("");
-                            At::Value => &model.textarea_input.value.as_deref().unwrap_or("")
-                        },
-                        C![C.input],
-                        input_ev(Ev::Input, Msg::FirstTextAreaChanged),
-                    ]
-                ],
-                div![
-                    id!["output"],
-                    C![C.mb_6, C.pt_3, C.rounded, C.bg_gray_200],
-                    label![C![C.input_label], &model.textarea_output.label],
-                    textarea![
-                        attrs! {
-                            At::Type => "text";
-                            At::Placeholder => &model.textarea_output.placeholder.as_deref().unwrap_or("");
-                            At::Value => &model.textarea_output.value.as_deref().unwrap_or("");
-                        },
-                        C![C.input],
-                        input_ev(Ev::Input, Msg::SecondTextAreaChanged),
-                    ]
-                ],
-                div![
-                    C![C.flex, C.justify_end],
-                    button![
-                        id!["swap-btn"],
-                        C![C.btn, C.mb_6, C.px_3, C.py_1, C.stroke_2],
-                        raw_svg!(icon::get(&icon::Name::SwitchVertical)),
-                        ev(Ev::Click, |_| Msg::Swap),
-                    ]
-                ],
-                button![
-                    id!["go-btn"],
-                    C![C.btn, C.inline_flex, C.justify_center, C.stroke_2],
-                    raw_svg!(icon::get(&icon::Name::Rocket)),
-                    span!["Go"],
-                    ev(Ev::Click, |_| Msg::Transform),
-                ],
-            ],
+            view_title(),
+            view_control_panel(&model.text_area_input, &model.text_area_output),
         ],
-        footer![
-            C![
-                C.max_w_lg,
-                C.mx_auto,
-                C.flex,
-                C.justify_center,
-                C.text_white,
-                C.font_medium
-            ],
-            a![
-                C![
-                    C.duration_500,
-                    C.transform,
-                    C.hover___translate_y_1,
-                    C.hover__scale_125,
-                    C.hover__underline,
-                ],
-                attrs! {At::Href => "#"},
-                "Support Me",
-                i![
-                    C![C.inline_block, C.mx_1, C.align_middle C.w_4, C.h_4],
-                    raw_svg!(icon::get(&icon::Name::SparklingHeart))
-                ],
-            ],
-            span![C![C.mx_3], "\u{2022}"],
-            a![C![C.hover__underline], attrs! {At::Href => "#"}, "Meta"],
+        view_footer()
+    ]
+}
+
+fn view_title() -> Node<Msg> {
+    section![p![
+        C![C.text_lg, C.text_center, C.text_gray_600, C.pt_0],
+        "Convert your TailwindCSS",
+        i![
+            C![C.inline_block, C.mx_1, C.align_middle C.w_4, C.h_4],
+            raw_svg!(icon::get(&icon::Name::Wind))
+        ],
+        "to typed Rust",
+        i![
+            C![C.inline_block, C.mx_1, C.align_middle C.w_4, C.h_4],
+            raw_svg!(icon::get(&icon::Name::Crab))
         ],
     ]]
+}
+
+fn view_control_panel(text_area_input: &Box<dyn TextArea>, text_area_output: &Box<dyn TextArea>) -> Node<Msg> {
+    section![
+        C![C.flex, C.flex_col, C.mt_10],
+        view_text_area(text_area_input, "input", Msg::TextAreaInputChanged),
+        view_text_area(text_area_output, "output", Msg::TextAreaOutputChanged),
+        div![
+            C![C.flex, C.justify_end],
+            button![
+                id!["swap-btn"],
+                C![C.btn, C.mb_6, C.px_3, C.py_1, C.stroke_2],
+                raw_svg!(icon::get(&icon::Name::SwitchVertical)),
+                ev(Ev::Click, |_| Msg::Swap),
+            ]
+        ],
+        button![
+            id!["go-btn"],
+            C![C.btn, C.inline_flex, C.justify_center, C.stroke_2],
+            raw_svg!(icon::get(&icon::Name::Rocket)),
+            span!["Go"],
+            ev(Ev::Click, |_| Msg::Transform),
+        ],
+    ]
+}
+
+fn view_text_area(
+    text_area: &Box<dyn TextArea>, 
+    id: &'static str, 
+    on_change: fn(String) -> Msg,
+) -> Node<Msg> {
+    div![
+        id![id],
+        C![C.mb_6, C.pt_3, C.rounded, C.bg_gray_200],
+        label![
+            C![C.input_label],
+            text_area.label(),
+        ],
+        textarea![
+            attrs! {
+                At::Type => "text",
+                At::Placeholder => text_area.placeholder(),
+                At::Value => text_area.value()
+            },
+            C![C.input],
+            input_ev(Ev::Input, on_change),
+        ]
+    ]
+}
+
+fn view_footer() -> Node<Msg> {
+    footer![
+        C![
+            C.max_w_lg,
+            C.mx_auto,
+            C.flex,
+            C.justify_center,
+            C.text_white,
+            C.font_medium
+        ],
+        a![
+            C![
+                C.duration_500,
+                C.transform,
+                C.hover___translate_y_1,
+                C.hover__scale_125,
+                C.hover__underline,
+            ],
+            attrs! {At::Href => "#"},
+            "Support Me",
+            i![
+                C![C.inline_block, C.mx_1, C.align_middle C.w_4, C.h_4],
+                raw_svg!(icon::get(&icon::Name::SparklingHeart))
+            ],
+        ],
+        span![C![C.mx_3], "\u{2022}"],
+        a![C![C.hover__underline], attrs! {At::Href => "#"}, "Meta"],
+    ]
 }
 
 // ------ ------
